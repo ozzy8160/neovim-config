7beb82636f6a20a6f9f239acfc9c69f513799d40
ozzy8160 <ryanpctech81@gmail.com>
2025-01-20 19:48:11 -0500

    initial commit

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+require("config.lazy")
+require("config.keymaps")
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"bash", "css", "dockerfile", "hyprlang", "lua", "luadoc", "nix", "markdown", "vim", "vimdoc", "xml", "yaml",},
+  sync_install = false,
+  highlight = {enable = true},
+  indent = {enable = true},
+})
+
+local harpoon = require("harpoon")
+harpoon:setup()
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+require("nightly").setup({
+  transparent = true,
+  styles = {
+    comments = { italic = true },
+    functions = { italic = false },
+    variables = { italic = false },
+    keywords = { italic = false },
+  },
+  highlights = {},
+})
---
+++ lua/plugins.lua
@@ -0,0 +1,29 @@
+return{
+
+    {
+    	"Alexis12119/nightly.nvim",
+    	lazy = false,
+    	priority = 1000,
+    	config = function()
+      		vim.cmd.colorscheme "nightly"
+    	end,
+    },
+
+    {
+	"nvim-lua/plenary.nvim",
+    	"mbbill/undotree",
+	"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    },
+
+    {
+	'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	dependencies = { "nvim-lua/plenary.nvim" }
+    },
+
+    {
+    	"ThePrimeagen/harpoon",
+    	branch = "harpoon2",
+    	dependencies = { "nvim-lua/plenary.nvim" }
+    },
+
+}
